099eef243f1667524d2383af1b8721cf97e28a00
trongsieucapvippro <94774477+trongsieucapvippro@users.noreply.github.com>
2023-05-21 14:40:27 +0700

    fix the stupid error wrong text

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -67,7 +67,7 @@ const SignupPage = () => {
       slug: slugify(values.username, { lower: true }),
       avatar:
         "https://images.unsplash.com/photo-1618005198919-d3d4b5a92ead?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
-      createtAt: serverTimestamp(),
+      createdAt: serverTimestamp(),
     });
     toast.success("Create user successfully !", {
       theme: "colored",
